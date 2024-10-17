820a5849e49cee930cbc09bb6138d927d4e729e2
HCPTangHY <tanghy06025@163.com>
2024-10-17 21:47:44 +0800

    modified:   paperdoll.js 	modified:   util.twee

--- paperdoll.js
+++ paperdoll.js
@@ -16,28 +16,53 @@ setup.paperdollPC = async function(canvas) {
     // 加载人模
     await p.loadBaseModel('res/body/basenoarms.png');
 
+    V.pc.get_clothingItems_classes();
+    let clothes = V.pc.clothes;
+    clothes.sort((a, b) => setup.clothes[a.item].layer - setup.clothes[b.item].layer);
     // 开始图层加载
     // 后发
-    // await p.loadLayer('res/bhair.png', '#F1C232');
+    await p.loadLayer(`res/hair/back/${V.pc['hair style'].replace(/ /g,'_')}/${V.pc['hair length']}.png`, V.pc['hair color']);
     // 身体(无手)
     await p.loadLayer('res/body/basenoarms.png');
-
-    // 下衣
-    // await p.loadLayer('res/trousers.png', '#FFFF00');
-    // 手
-    await p.loadLayer('res/body/arms.png');
-
-    if (V.pc.has_part("penis")) {
-        await p.loadLayer(V.pc.virgin() ? `res/body/penis/penis_virgin${Math.floor(V.pc['penis size']/200)-2}.png` : `res/body/penis/penis${Math.floor(V.pc['penis size']/200)-2}.png`);
-    } else if (V.pc.has_part("breasts")) {
-        await p.loadLayer(`res/body/breasts/breasts${Math.floor(V.pc['breast size']/200)}.png`)
-    }
     // 头
     await p.loadLayer('res/body/basehead.png');
+    // 眼睛
+    await p.loadLayer('res/face/eyes.png', setup.color_table[V.pc['eye color']]);
+
+    // 身体衣服
+    for (let i = 0; i < clothes.length; i++) {
+        let citem = setup.clothes[clothes[i].item];
+        if (citem.name.indexOf('color2') !== -1) {
+            await p.loadLayer(`res/clothes/${citem.category}/${clothes[i].item.replace(/ /g,'_')}/full_gray.png`, setup.color_table[clothes[i].subs['color1']] ? setup.color_table[clothes[i].subs['color1']] : clothes[i].subs['color1']);
+            await p.loadLayer(`res/clothes/${citem.category}/${clothes[i].item.replace(/ /g,'_')}/color2_gray.png`, setup.color_table[clothes[i].subs['color2']] ? setup.color_table[clothes[i].subs['color2']] : clothes[i].subs['color2']);
+        } else {
+            await p.loadLayer(`res/clothes/${citem.category}/${clothes[i].item.replace(/ /g,'_')}/full_gray.png`, setup.color_table[clothes[i].subs['color']] ? setup.color_table[clothes[i].subs['color1']] : clothes[i].subs['color1']);
+        }
+    }
+    // 手
+    await p.loadLayer('res/body/leftarm.png');
+
+    if (V.pc.has_part("penis") && V.pc.is_part_visible('penis')) {
+        await p.loadLayer(V.pc.virgin() ? `res/body/penis/penis_virgin${Math.floor(V.pc['penis size']/200)-2}.png` : `res/body/penis/penis${Math.floor(V.pc['penis size']/200)-2}.png`);
+    } else if (V.pc.has_part("breasts") && V.pc.is_part_visible('nipples')) {
+        await p.loadLayer(`res/body/breasts/breasts${Math.floor(V.pc['breast size']/200)}.png`)
+    }
+
+    // 左手衣服
+    for (let i = 0; i < clothes.length; i++) {
+        let citem = setup.clothes[clothes[i].item];
+        await p.loadLayer(`res/clothes/${citem.category}/${clothes[i].item.replace(/ /g,'_')}/left_gray.png`, setup.color_table[clothes[i].subs['color']]);
+    }
+    // 右手
+    await p.loadLayer('res/body/rightarm.png');
+    // 右手衣服
+    for (let i = 0; i < clothes.length; i++) {
+        let citem = setup.clothes[clothes[i].item];
+        await p.loadLayer(`res/clothes/${citem.category}/${clothes[i].item.replace(/ /g,'_')}/right_gray.png`, setup.color_table[clothes[i].subs['color']]);
+    }
+
     // 前发
-    // await p.loadLayer('res/fhair.png', '#F1C232');
-    // 上衣
-    // await p.loadLayer('res/tshirt.png', '#FFA500')
+    await p.loadLayer(`res/hair/front/${V.pc['hair style'].replace(/ /g,'_')}/${V.pc['hair length']}.png`, V.pc['hair color']);
 
     setTimeout(function() {
         console.log('All layers loaded');
