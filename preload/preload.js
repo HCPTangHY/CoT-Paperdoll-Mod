fe6d429c539bd56852bbf8348debe25d4db7ac18
HCPTangHY <tanghy06025@163.com>
2024-11-06 22:43:28 +0800

    0.2.0

---
+++ preload/preload.js
@@ -0,0 +1,5 @@
+(async () => {
+    // register mod at there to let this mod be loaded as the latest in `ImgLoaderHooker.addSideHooker`
+    window.PaperdollOriginalImagePack.init();
+})();
+//# sourceMappingURL=preload.js.map
